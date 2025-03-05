b2a2bdd8b76d0d1a347dce0a3b73ec2a793cd790
Christian Arias <carias@elpuig.xeill.net>
2025-03-05 16:59:08 +0100

    Test

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,21 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        method1();
+        method2();
+        method3();
+        method4();
+    }
+    private static void method1(){
 
-        System.out.println("Hello World");
+    }
+    private static void method2(){
+
+    }
+    private static void method3(){
+
+    }
+    private static void method4(){
+
     }
 }
